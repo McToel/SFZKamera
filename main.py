f3497940876c1803989fcf4697df44e8f832abf7
McToel <36071676+McToel@users.noreply.github.com>
2019-09-20 17:34:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 #import cv2
 import time
+import serial
 from picamera import PiCamera
 
+
 #camera = cv2.VideoCapture(0)
+camera = PiCamera()
+
+arduino = serial.Serial('/dev/ttyACM0', baudrate = 9600, timeout = 10)
+time.sleep(10) #wait for the arduino to initialize
+
+print("running")
 pictureNumber = 0
 while True:
     #picam
     #region
+    arduino.write('ON'.encode('ascii'))
     camera.start_preview()
-    time.sleep(30)
+    time.sleep(5)
     camera.capture('img%s.jpg'%('{0:05d}'.format(pictureNumber)))
+    camera.stop_preview()
+    arduino.write('OFF'.encode('ascii'))
+    time.sleep(115)
     #endregion 
 
     #cv2
