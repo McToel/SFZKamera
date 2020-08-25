694349f253808b49d96c15cf6905d6ee3b805e47
McToel <theo.doellmann@gmx.de>
2020-08-25 17:37:48 +0200

    changed png to jpg

--- server.py
+++ server.py
@@ -6,8 +6,8 @@ app.secret_key = os.urandom(12)
 
 @app.route('/')
 def favicon():
-    app.send_static_file('latest.png')
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'latest.png')
+    app.send_static_file('latest.jpg')
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'latest.jpg')
 
 if __name__ == "__main__":
     # app.run(host='0.0.0.0', port=5000)
