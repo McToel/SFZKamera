6e465b3059736d0b79136aa33b64af9da9c912ad
McToel <theo.doellmann@gmx.de>
2020-09-09 14:57:54 +0200

    Changed timing

--- plantcamera.py
+++ plantcamera.py
@@ -15,18 +15,16 @@ start_time = time.time()
 
 print("running")
 pictureNumber = 0
-last_time = start_time
 while True:
-    if (time.time() - last_time) > 60 * 5:
-        last_time = time.time()
-        arduino.write('ON'.encode('ascii'))
-        camera.start_preview()
-        time.sleep(10)
-        camera.capture('images/img{0:016d}.jpg'.format(int(time.time() - start_time)))
-        camera.capture('static/latest.jpg')
-        camera.stop_preview()
-        arduino.write('OFF'.encode('ascii'))
+    last_time = time.time()
+    arduino.write('ON'.encode('ascii'))
+    camera.start_preview()
+    time.sleep(10)
+    camera.capture('images/img{0:016d}.jpg'.format(int(time.time() - start_time)))
+    camera.capture('static/latest.jpg')
+    camera.stop_preview()
+    arduino.write('OFF'.encode('ascii'))
 
-        pictureNumber += 1
-    else:
-        time.sleep(0.5)
+    pictureNumber += 1
+
+    time.sleep((60 * 5) - ((time.time() - start_time) % (60 * 5)))
